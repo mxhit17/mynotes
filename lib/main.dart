e3d803ecaf91e77f7b15b84d33f20cb13bd3ecea
mxhit17 <limmudgal@gmail.com>
2024-07-15 19:30:11 +0530

    corrected everything

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
-<<<<<<< HEAD
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verifyemail_view.dart';
-=======
->>>>>>> origin/master
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +15,10 @@ void main() {
       useMaterial3: true,
     ),
     home: const HomePage(),
-<<<<<<< HEAD
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/': (context) => const RegisterView(),
     },
-=======
->>>>>>> origin/master
   ));
 }
 
@@ -33,7 +27,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return FutureBuilder(
       // The future parameter expects a Future object. This Future will eventually provide a result or an error.
       future: Firebase.initializeApp(
@@ -46,69 +39,18 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                // print('Email is verified.');
-                return const NotesView();
+                print('Email is verified.');
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-          // return const Text('Done');
+            return const Text('Done');
           default:
             return const Text('Loading...');
         }
       },
-    );
-  }
-}
-
-enum MenuAction { logout }
-
-class NotesView extends StatefulWidget {
-  const NotesView({super.key});
-
-  @override
-  State<NotesView> createState() => _NotesViewState();
-}
-
-class _NotesViewState extends State<NotesView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MAIN UI'),
-      ),
-      body: const Text('Hello world'),
-=======
-    return Scaffold(
-      // The Scaffold is a widget in Flutter used to implements the basic material design visual layout structure.
-      appBar: AppBar(
-        // It is a horizontal bar that is mainly displayed at the top of the Scaffold widget.
-        title: const Text('Login Window'),
-      ),
-      body: FutureBuilder(
-        // The future parameter expects a Future object. This Future will eventually provide a result or an error.
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        //  The builder function is called whenever the state of the Future changes. This function receives two parameters: the BuildContext and an AsyncSnapshot.
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('you are a verified user.');
-              } else {
-                print('please verify your email.');
-              }
-              return Text('Done.');
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
->>>>>>> origin/master
     );
   }
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,11 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
-=======
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:mynotes/firebase_options.dart';
->>>>>>> origin/master
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -36,7 +30,6 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       appBar: AppBar(
         title: const Text('Register win'),
       ),
@@ -85,68 +78,6 @@ class _RegisterViewState extends State<RegisterView> {
               },
               child: const Text('Already registered? Login here!')),
         ],
-=======
-      // The Scaffold is a widget in Flutter used to implements the basic material design visual layout structure.
-      appBar: AppBar(
-        // It is a horizontal bar that is mainly displayed at the top of the Scaffold widget.
-        title: const Text('Register Window'),
-      ),
-      body: FutureBuilder(
-        // The future parameter expects a Future object. This Future will eventually provide a result or an error.
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        //  The builder function is called whenever the state of the Future changes. This function receives two parameters: the BuildContext and an AsyncSnapshot.
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                // vertial layout (stacked on top of each other).
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration:
-                        const InputDecoration(hintText: 'Enter your Email'),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration:
-                        const InputDecoration(hintText: 'Enter your Password'),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak-password') {
-                          print('weak passcode');
-                        } else if (e.code == 'email-already-in-use') {
-                          print('email already registered');
-                        } else if (e.code == 'invalid-email') {
-                          print('invalid email bro');
-                        }
-                      }
-                    },
-                    child: const Text('Register'),
-                  ),
-                ],
-              );
-            default:
-              return const Text('Loading...');
-          }
-        },
->>>>>>> origin/master
       ),
     );
   }
